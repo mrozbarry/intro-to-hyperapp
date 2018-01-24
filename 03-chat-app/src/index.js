2ff559328e43f684d7017332200814b853a3af28
Alex Barry <alex.barry@gmail.com>
2018-01-23 20:48:32 -0500

    Made the chat app look generally better, but still needs some work.

--- 03-chat-app/src/index.js
+++ 03-chat-app/src/index.js
@@ -1,5 +1,6 @@
 import { h, app } from 'hyperapp'
 import * as firebase from 'firebase'
+import randomcolor from 'randomcolor'
 
 firebase.initializeApp({
   apiKey: "AIzaSyBQWJQT29mFsSwlGDWXrgIwgVYddpDQ8aQ",
@@ -11,6 +12,7 @@ firebase.initializeApp({
 const state = {
   user: null,
   name: 'User',
+  users: {},
   messages: [],
   message: '',
 }
@@ -24,34 +26,55 @@ const actions = {
         return firebase.database()
           .ref("users")
           .child(user.uid)
-          .set(name)
-      })
+          .set(name);
+      });
   },
 
   logout: () => (state, action) => {
-    firebase.auth().signOut()
+    return firebase.database()
+      .ref('users')
+      .child(state.user.uid)
+      .remove()
+      .then(() => {
+        firebase.auth().signOut();
+      })
   },
 
-  setAuth: user => state => ({
-    user: user,
-    name: state.name,
-    users: state.users,
-    messages: state.messages,
-    message: ''
-  }),
+  setAuth: user => state => {
+    const chatUser = user !== null
+      ? state.users[user.uid]
+      : null;
 
-  addMessage: message => state => ({
+    return {
+      user: user,
+      name: user && chatUser ? chatUser.name : state.name,
+      users: state.users,
+      messages: state.messages,
+      message: '',
+    };
+  },
+
+  addMessage: message => state => {
+    const user = state.users[message.userId];
+    const messageWithUser = Object.assign({ user: user }, message);
+
+    const messages = state.messages
+      .concat(messageWithUser)
+      .sort((a, b) => Math.sign(a.createdAt - b.createdAt))
+
+    return {
+      user: state.user,
+      name: state.name,
+      users: state.users,
+      messages: messages,
+      message: state.message,
+    };
+  },
+
+  mergeUser: user => state => ({
     user: state.user,
     name: state.name,
-    users: state.users,
-    messages: state.messages.concat(message).sort((a, b) => Math.sign(a.createdAt - b.createdAt)),
-    message: state.message,
-  }),
-
-  setUsers: users => state => ({
-    user: state.user,
-    name: state.name,
-    users: users,
+    users: Object.assign({}, state.users, { [user.id]: user }),
     messages: state.messages,
     message: state.message,
   }),
@@ -100,47 +123,67 @@ const actions = {
 }
 
 const view = (state, actions) => (
-  <div>
-    <input
-      placeholder="Username"
-      value={state.name}
-      disabled={Boolean(state.user)}
-      oninput={(e) => actions.nameChanged(e.target.value)}
-      onkeydown={(e) => {
-        if (e.which === 13) {
-          console.log('do that login')
-          e.preventDefault()
-          actions.login(state.name)
+  <div class="layout">
+    <div class="auth-bar">
+      <input
+        class="auth-bar-name"
+        placeholder="Username"
+        value={state.name}
+        disabled={Boolean(state.user)}
+        oninput={(e) => actions.nameChanged(e.target.value)}
+      />
+      <button class="auth-bar-button" onclick={() => actions.login(state.name)} disabled={Boolean(state.user)}>
+        Login
+      </button>
+      <button class="auth-bar-button" onclick={() => actions.logout()} disabled={!Boolean(state.user)}>
+        Logout
+      </button>
+    </div>
+
+    <div class="messages">
+      {state.messages.map((message, idx) => {
+        const isFromMe = state.user && message.userId === state.user.uid
+        const style = {
+          alignSelf: isFromMe ? 'flex-end' : 'flex-start',
+          backgroundColor: message.user.color,
         }
-      }}
-    />
-    <button onclick={() => actions.logout()} disabled={!Boolean(state.user)}>
-      Logout
-    </button>
-    <input
-      value={state.message}
-      disabled={!Boolean(state.user)}
-      oninput={(e) => actions.messageChanged(e.target.value)}
-      onkeydown={(e) => {
-        if (e.which === 13) {
-          e.preventDefault()
-          actions.sendMessage(state.message)
-        }
-      }}
-      placeholder="Add your message"
-    />
-    <div>{state.messages.map((message, idx) => {
-      return (
-        <div key={message.id}>
-          <div>[{(new Date(message.createdAt)).toISOString()}] {message.userId}:</div>
-          {message.text}
-        </div>
-      );
-    })}</div>
+        return (
+          <div key={message.id} class="message" style={style}>
+            <div class="message-author">{message.user.name}</div>
+            <div class="message-body">{message.text}</div>
+            <div class="message-date">{(new Date(message.createdAt)).toISOString()}</div>
+          </div>
+        );
+      })}
+    </div>
+    <div class="my-message">
+      <input
+        value={state.message}
+        disabled={!Boolean(state.user)}
+        oninput={(e) => actions.messageChanged(e.target.value)}
+        onkeydown={(e) => {
+          if (e.which === 13) {
+            e.preventDefault()
+            actions.sendMessage(state.message)
+          }
+        }}
+        placeholder="Add your message"
+      />
+    </div>
   </div>
 );
 
 const chat = app(state, actions, view, document.getElementById('root'));
+
+firebase.database()
+  .ref("users")
+  .on("child_added", (snapshot) => {
+    chat.mergeUser({
+      id: snapshot.key,
+      name: snapshot.val(),
+      color: randomcolor({ luminosity: 'bright', format: 'rgba', alpha: 0.8 })
+    })
+  });
 
 firebase.auth()
   .onAuthStateChanged((user) => {
@@ -154,14 +197,3 @@ firebase.database()
     chat.addMessage(v);
   });
 
-firebase.database()
-  .ref("users")
-  .on("value", (snapshot) => {
-    const users = snapshot.val();
-    if (!users) return chat.setUsers([]);
-
-    chat.setUsers(
-      Object.keys(users)
-        .map((id) => ({ id: id, name: users[id] }))
-    );
-  });
